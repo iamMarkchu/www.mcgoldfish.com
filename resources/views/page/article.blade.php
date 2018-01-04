19b71f3a28f95f7dc21dcac863ea54e6cd593922
mark <454085441@qq.com>
2018-01-04 15:55:20 +0800

    no message

--- resources/views/page/article.blade.php
+++ resources/views/page/article.blade.php
@@ -12,22 +12,27 @@
     </div>
     <div class="col-md-3 hidden-sm">
         <div class="article-menu affix-top" data-spy="affix" data-offset-top="0" data-offset-bottom="10">
-            <span>目录</span>
-            <ul class="header-list">
-                @foreach($headers as $index => $header)
-                    <li>
-                        <a href="#{{ $index }}">{{ $header['title'] }}</a>
-                        @if(isset($header['h3']))
-                            <ol class="sub-header-list">
-                                @foreach($header['h3'] as $subIndex => $subHeader)
-                                    <li><a href="#{{ $subIndex }}">{{ $subHeader }}</a></li>
-                                @endforeach
-                            </ol>
-                        @endif
-                    </li>
-                @endforeach
-                <li><a href="#header_1">返回顶部</a></li>
-            </ul>
+            <div class="panel panel-default">
+                <div class="panel-heading">目录</div>
+                <div class="panel-body">
+                    <ul class="header-list">
+                        @foreach($headers as $index => $header)
+                            <li>
+                                <a href="#{{ $index }}">{{ $header['title'] }}</a>
+                                @if(isset($header['h3']))
+                                    <ol class="sub-header-list">
+                                        @foreach($header['h3'] as $subIndex => $subHeader)
+                                            <li><a href="#{{ $subIndex }}">{{ $subHeader }}</a></li>
+                                        @endforeach
+                                    </ol>
+                                @endif
+                            </li>
+                        @endforeach
+                        <li><a href="#header_1">返回顶部</a></li>
+                    </ul>
+                </div>
+            </div>
+
         </div>
     </div>
 @endsection
