8eb52ac03a652aee78f3a82aa02a7f920329f995
mark <454085441@qq.com>
2018-01-17 17:40:53 +0800

    no message

--- app/Http/Controllers/Page/HomeController.php
+++ app/Http/Controllers/Page/HomeController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Article;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Tools\Baidu\Translate;
 
 class HomeController extends Controller
 {
